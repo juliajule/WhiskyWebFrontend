382a07c888e9687fae4056e84f2ab7996fe0b738
Julia Puhlmann <107950859+juliajule@users.noreply.github.com>
2025-06-30 20:18:07 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-const BASE_URL = 'http://192.168.1.197:8080';
+const BASE_URL = 'http://home.komtur.org:8542';
 const APP_VERSION = '1.0.0';
-const LOCAL_URL =  'http://localhost:8080';
+const LOCAL_URL =  'http://home.komtur.org:8542';
